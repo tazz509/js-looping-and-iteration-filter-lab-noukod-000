33fc36d69c6f82ce6a57dcb6304d46043cec409f
carly <carly@noukod.com>
2019-02-28 17:22:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, word){
+    return drivers.filter(driver => driver.toLowerCase() == word.toLowerCase());
+}
+
+function fuzzyMatch(drivers, partialWrold){
+    return drivers.filter(function (driver){return driver.slice(0, partialWrold.length) == partialWrold});
+}
+
+function matchName(drivers, word){
+    return drivers.filter(function (driver){return driver["name"].match(word)});
+}
